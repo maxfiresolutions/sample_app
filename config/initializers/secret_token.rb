6062dcd121afe23caa9002ecd45fdbef60c9865e
nauman <maxfiresolutions@gmail.com>
2014-02-06 07:45:44 +0500

    Initial Commit
    The first commit after making changes to the Gem file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '9245437bef29b8b935ae16e3249d8de27a77f90beeee10f2e5e46d8e49f1490790bed0064cd62207f88fa7228c483901797aab7a052c37cedaf23dab8be8f500'
